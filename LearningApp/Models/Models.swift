89f52f1a269ad5533933f7ec9e5df5f0fa8cacb1
Balazs Kepli <keplib@gmail.com>
2022-01-07 14:19:22 +0100

    Adding data models Adding view model Adding JSON Parser Adding parser to get HTML / CSS

---
+++ LearningApp/Models/Models.swift
@@ -0,0 +1,54 @@
+//
+//  Models.swift
+//  LearningApp
+//
+//  Created by Balázs Képli on 7/1/22.
+//
+
+import Foundation
+
+struct Module: Decodable, Identifiable {
+    
+    var id: Int
+    var category: String
+    var content: Content
+    var test: Test
+}
+
+struct Content:Decodable,  Identifiable {
+    
+    var id: Int
+    var image:String
+    var time: String
+    var description: String
+    var lessons: [Lesson]
+    
+    
+}
+
+struct Lesson: Decodable, Identifiable {
+    var id: Int
+    var title: String
+    var video: String
+    var duration: String
+    var explanation: String
+    
+    
+}
+
+struct Test: Decodable, Identifiable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var questions: [Question]
+}
+
+struct Question: Identifiable, Decodable {
+    
+    var id: Int
+    var content: String
+    var correctIndex: Int
+    var answers: [String]
+}
--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,4 +9,52 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    var styleData: Data?
+    
+    init() {
+        getLocalData()
+    }
+    
+    // Parsing the JSON
+    
+    func getLocalData() {
+        
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                let modules =  try jsonDecoder.decode([Module].self, from: jsonData)
+            }
+            catch {
+                print(error)
+            }
+            self.modules = modules
+        }
+        catch {
+            print(error)
+        }
+       
+        
+    // Parsing HTML / CSS
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+            
+            
+        } catch {
+            print(error)
+        }
+
+        
+    }
+    
 }
