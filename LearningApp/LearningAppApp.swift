737a2b6a9435ff0ea5434571728dbad9e3e46b40
Balazs Kepli <keplib@gmail.com>
2022-01-07 13:00:20 +0100

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Balázs Képli on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
